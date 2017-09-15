7b7db1347904b5c914ac3222f0b6395a680004dc
Dereje Kitaw <dkitaw@solareworld.local>
2017-09-15 16:25:34 -0400

    number variable created

--- types.js
+++ types.js
@@ -1,4 +1,7 @@
 // console.log('Hello from ts');
 var myString;
+var myNum;
 myString = 'hello world';
+myNum = 33;
 console.log(myString);
+console.log(myNum);
--- types.ts
+++ types.ts
@@ -1,6 +1,9 @@
 // console.log('Hello from ts');
 
 let myString: string;
+let myNum: number;
 
 myString='hello world';
+myNum=33;
 console.log(myString);
+console.log(myNum);
