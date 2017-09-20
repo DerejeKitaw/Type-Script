3d0a1830e8cb852b4e35647df54277c880ec7970
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-09-20 18:42:26 -0400

    adding strings by parsing to integer

--- functions.js
+++ functions.js
@@ -3,6 +3,12 @@ function getSum(num1, num2) {
 }
 console.log('getSum = ' + getSum(1, 4));
 var mySum = function (num1, num2) {
+    if (typeof num1 == 'string') {
+        num1 = parseInt(num1);
+    }
+    if (typeof num2 == 'string') {
+        num2 = parseInt(num2);
+    }
     return num1 + num2;
 };
-console.log('mySum = ' + mySum('1', 4));
+console.log('mySum = ' + mySum('1', '5'));
--- functions.ts
+++ functions.ts
@@ -4,6 +4,12 @@ function getSum(num1:number, num2:number):number{
 console.log('getSum = '+ getSum(1,4));
 
 let mySum =function(num1:any, num2:any){
+    if (typeof num1 == 'string'){
+        num1=parseInt(num1);
+    }
+    if (typeof num2 == 'string'){
+        num2=parseInt(num2);
+    }
     return num1 + num2;
 }
-console.log('mySum = '+mySum('1',4));
+console.log('mySum = '+mySum('1','5'));
