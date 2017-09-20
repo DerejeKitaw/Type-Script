b460e80e06e3c3277d5db620d80380eeb1ad100a
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-09-20 19:32:29 -0400

    How to use inheritance in a class

--- classes.js
+++ classes.js
@@ -1,3 +1,13 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var User = /** @class */ (function () {
     function User(name, email, age) {
         this.name = name;
@@ -6,8 +16,11 @@ var User = /** @class */ (function () {
         console.log('User Created:' + this.name);
     }
     //Creating methods for the class
-    User.prototype.registor = function () {
+    User.prototype.register = function () {
         console.log(this.name + ' is now registered');
+    };
+    User.prototype.payInvoice = function () {
+        console.log(this.name + ' paid invoice');
     };
     return User;
 }());
@@ -15,4 +28,18 @@ var User = /** @class */ (function () {
 var dereje = new User('Dereje Kitaw', 'dereje.kitaw@gmail.com', 40);
 // if age is private we can not access it outside the class. by default it is public.
 console.log(dereje.age);
-dereje.registor();
+dereje.register();
+var Member = /** @class */ (function (_super) {
+    __extends(Member, _super);
+    function Member(id, name, email, age) {
+        var _this = 
+        // supper must have parameters which is inherenting from other class
+        _super.call(this, name, email, age) || this;
+        _this.id = id;
+        return _this;
+    }
+    Member.prototype.payinvoice = function () {
+        _super.prototype.payInvoice.call(this);
+    };
+    return Member;
+}(User));
--- classes.ts
+++ classes.ts
@@ -1,5 +1,4 @@
 class User{
-
     name: string;
     email:string;
     age:number;
@@ -14,8 +13,11 @@ class User{
     }
 
     //Creating methods for the class
-    registor(){
+    register(){
         console.log(this.name + ' is now registered');
+    }
+    payInvoice(){
+        console.log(this.name + ' paid invoice');
     }
 }
 
@@ -25,4 +27,19 @@ let dereje = new User('Dereje Kitaw','dereje.kitaw@gmail.com',40);
 // if age is private we can not access it outside the class. by default it is public.
 console.log (dereje.age);
 
-dereje.registor();
+dereje.register();
+
+
+class Member extends User{
+    id:number;
+
+    constructor(id:number,name:string,email:string,age:number){
+        // supper must have parameters which is inherenting from other class
+        super(name,email,age); //this is like making this.name=name;....
+        this.id=id;
+    }
+
+    payinvoice(){
+        super.payInvoice();
+    }
+}
