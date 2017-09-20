31769aaf066ba4de0ae5a2a2703ad527e0ee649f
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-09-20 19:37:15 -0400

    using interface for the class

--- classes.ts
+++ classes.ts
@@ -1,4 +1,11 @@
-class User{
+interface UserInterface{
+    name: string;
+    email:string;
+    age:number;
+    register();
+    payInvoice();
+}
+class User implements   UserInterface{
     name: string;
     email:string;
     age:number;
