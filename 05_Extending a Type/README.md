3588263d2b3ac4453dd479dc2b5c7408bd6a6480
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-11-26 16:26:34 -0500

    extending types

---
+++ 05_Extending a Type/README.md
@@ -0,0 +1,13 @@
+# 01_Child class extends Parent class
+
+    class ChildClass extends ParnetClass {}
+
+# 02_To call base class constructor use super();
+
+    class ChildClass extends ParnetClass {
+        constructor() {
+            super(); // will call ParentClass constructor
+        }
+    }
+
+    Eg. 
---
+++ 05_Extending a Type/auto.js
@@ -0,0 +1,113 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Engine = /** @class */ (function () {
+    function Engine(horsePower, engineType) {
+        this.horsePower = horsePower;
+        this.engineType = engineType;
+    }
+    Engine.prototype.start = function (callback) {
+        var _this = this;
+        window.setTimeout(function () {
+            callback(true, _this.engineType);
+        }, 1000);
+    };
+    Engine.prototype.stop = function (callback) {
+        var _this = this;
+        window.setTimeout(function () {
+            callback(true, _this.engineType);
+        }, 1000);
+    };
+    return Engine;
+}());
+var Accessory = /** @class */ (function () {
+    function Accessory(accessoryNumber, title) {
+        this.accessoryNumber = accessoryNumber;
+        this.title = title;
+    }
+    return Accessory;
+}());
+var Auto = /** @class */ (function () {
+    function Auto(basePrice, engine, make, model) {
+        this.engine = engine;
+        this.basePrice = basePrice;
+        this.make = make;
+        this.model = model;
+    }
+    Auto.prototype.calculateTotal = function () {
+        var taxRate = .08;
+        return this.basePrice + (taxRate * this.basePrice);
+    };
+    Auto.prototype.addAccessories = function () {
+        var accessories = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            accessories[_i] = arguments[_i];
+        }
+        this.accessoryList = '';
+        for (var i = 0; i < accessories.length; i++) {
+            var ac = accessories[i];
+            this.accessoryList += ac.accessoryNumber + ' ' + ac.title + '<br />';
+        }
+    };
+    Auto.prototype.getAccessoryList = function () {
+        return this.accessoryList;
+    };
+    Object.defineProperty(Auto.prototype, "basePrice", {
+        get: function () {
+            return this._basePrice;
+        },
+        set: function (value) {
+            if (value <= 0)
+                throw 'price must be >= 0';
+            this._basePrice = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Auto.prototype, "engine", {
+        get: function () {
+            return this._engine;
+        },
+        set: function (value) {
+            if (value == undefined)
+                throw 'Please supply an engine.';
+            this._engine = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Auto;
+}());
+var Truck = /** @class */ (function (_super) {
+    __extends(Truck, _super);
+    function Truck(basePrice, engine, make, model, bedlength, fourByFour) {
+        var _this = _super.call(this, basePrice, engine, make, model) || this;
+        _this.bedLength = bedlength;
+        _this.fourByFour = fourByFour;
+        return _this;
+    }
+    return Truck;
+}(Auto));
+window.onload = function () {
+    // var auto = new Auto(40000, new Engine(300, 'V8'), 'Chevy', 'silverado');
+    // alert(auto.engine.engineType);
+    // var truck = new Truck(40000, new Engine(300, 'V8'), 'Chevy', 'silverado','Long Bed',true);
+    // alert(truck.engine.engineType);
+    // alert(truck.bedLength);
+    // alert(truck.basePrice);
+    // alert(truck.calculateTotal().toString());
+    var truck = new Truck(40000, new Engine(300, 'V8'), 'Chevy', 'silverado', 'Long Bed', true);
+    // the ... allow to have unlimited parameter to Accossory
+    truck.addAccessories(new Accessory(1234, 'Sunroof'), new Accessory(4322, 'Towing package'));
+    // start is like a call back function
+    truck.engine.start(function (status, engineType) {
+        alert(engineType + ' was started');
+    });
+};
